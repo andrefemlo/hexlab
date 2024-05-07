ea18dcdbbf7079edc1bced461c10d42785ebf910
andrefemlo <andrefemlo@gmail.com>
2024-05-06 21:55:28 -0300

    added Vercel SpeedInsights

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -1,3 +1,7 @@
+//Vercel SpeedInsights
+import { injectSpeedInsights } from '../@vercel/speed-insights';
+injectSpeedInsights();
+
 // AOS
 AOS.init({
   duration: 1000,
@@ -108,46 +112,46 @@ function closeMobileSidebar() {
 // openSubmenu();
 
 
-function aceitarCookies() {
-  var dataExpiracao = new Date();
-  dataExpiracao.setDate(dataExpiracao.getDate() + 30);
+// function aceitarCookies() {
+//   var dataExpiracao = new Date();
+//   dataExpiracao.setDate(dataExpiracao.getDate() + 30);
   
-  document.cookie = "cookiesAceitos=true; expires=" + dataExpiracao.toUTCString() + "; path=/";
-  document.getElementById("cookieNotice").style.display = "none";
-}
+//   document.cookie = "cookiesAceitos=true; expires=" + dataExpiracao.toUTCString() + "; path=/";
+//   document.getElementById("cookieNotice").style.display = "none";
+// }
 
-function verificarCookiesAceitos() {
-  var cookiesAceitos = document.cookie.includes("cookiesAceitos=true");
-  if (!cookiesAceitos) {
-    document.getElementById("cookieNotice").style.display = "block";
-  } else {
-    document.getElementById("cookieNotice").style.display = "none";
-  }
-}
+// function verificarCookiesAceitos() {
+//   var cookiesAceitos = document.cookie.includes("cookiesAceitos=true");
+//   if (!cookiesAceitos) {
+//     document.getElementById("cookieNotice").style.display = "block";
+//   } else {
+//     document.getElementById("cookieNotice").style.display = "none";
+//   }
+// }
 
-window.onload = verificarCookiesAceitos
+// window.onload = verificarCookiesAceitos
 
 // Acordeão
-document.addEventListener("DOMContentLoaded", function () {
-  var accordion = document.querySelectorAll(".accordion");
+// document.addEventListener("DOMContentLoaded", function () {
+//   var accordion = document.querySelectorAll(".accordion");
 
-  accordion.forEach((el) => {
-    var accordionItems = el.querySelectorAll(".accordion-item");
+//   accordion.forEach((el) => {
+//     var accordionItems = el.querySelectorAll(".accordion-item");
 
-    accordionItems.forEach(function (item) {
-      var header = item.querySelector(".accordion-header");
+//     accordionItems.forEach(function (item) {
+//       var header = item.querySelector(".accordion-header");
 
-      header.addEventListener("click", function () {
-        var currentActiveItem = item.classList.contains("active");
+//       header.addEventListener("click", function () {
+//         var currentActiveItem = item.classList.contains("active");
 
-        accordionItems.forEach(function (el) {
-          el.classList.remove("active");
-        });
+//         accordionItems.forEach(function (el) {
+//           el.classList.remove("active");
+//         });
 
-        if (!currentActiveItem) {
-          item.classList.add("active");
-        }
-      });
-    });
-  });
-});
+//         if (!currentActiveItem) {
+//           item.classList.add("active");
+//         }
+//       });
+//     });
+//   });
+// });
